293149e892ab5d70482bb8e95a44498d27c0f3d4
Alex <alexandre.alm18@gmail.com>
2025-06-09 15:49:55 +0200

    fix back

--- app/__init__.py
+++ app/__init__.py
@@ -28,8 +28,6 @@ def create_app(config_class='app.config.Config'):
     api.init_app(app)
     
     # Enregistrement des blueprints
-    from app.controllers.user_controller import user_bp
-    app.register_blueprint(user_bp)
     from app.controllers.tesla_history_controller import bp as tesla_history_bp
     app.register_blueprint(tesla_history_bp)
     from app.controllers.graph_controller import graph_bp
